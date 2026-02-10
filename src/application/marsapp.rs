7e4c516289943369bebc70435aff8e3cf5a037ce
Joshua Smith <joshbear367763@gmail.com>
2026-02-10 00:11:57 -0500

    Applied new function, but now there's a problem where it opens the (right) neighbor's dropdown when you go to inspect a dropdown...

--- src/application/marsapp.rs
+++ src/application/marsapp.rs
@@ -1,7 +1,8 @@
 use eframe::{App, Frame};
-use egui::{Context, Id, LayerId, Ui};
+use egui::{Context, Id, Label, LayerId, Ui, Widget};
 use core::default::Default;
 use eframe::epaint::Color32;
+use crate::application::helper::StickyPopupExt;
 
 pub(crate) struct MarsApp
 {
@@ -21,30 +22,99 @@ impl MarsApp
 		egui::TopBottomPanel::top("top_panel")
 			.show(ctx, |ui: &mut Ui| {
 				egui::MenuBar::new().ui(ui, |ui: &mut Ui| {
-					ui.button("File").context_menu(|ui: &mut Ui| {});
-					ui.button("Edit");
-					ui.button("Run");
-					ui.button("Settings");
-					Self::create_tools_section(ui)
+					Self::create_file_section(ui);
+					Self::create_edit_section(ui);
+					Self::create_run_section(ui);
+					Self::create_settings_section(ui);
+					Self::create_tools_section(ui);
+					Self::create_help_section(ui);
 				})
 			});
 	} // create_toolbar
-	
+
+	fn create_file_section(ui : &mut Ui) {
+		let file_button = ui.button("File");
+		file_button.create_popup(ui, |ui : &mut Ui| {
+			ui.label("New");
+			ui.label("Open...");
+			ui.label("Close");
+			ui.label("Close All");
+			// TODO: investigate putting a line here.
+			ui.label("Save");
+			ui.label("Save As...");
+			ui.label("Save All");
+			ui.label("Dump Memory...");
+			// TODO: Another line here
+			ui.label("Print...");
+			// TODO: Investigate if this applies to web...
+			ui.label("Exit");
+		})
+	} // create_file_section
+
+	fn create_edit_section(ui : &mut Ui) {
+		ui.button("Edit").create_popup(ui, |ui : &mut Ui| {
+			ui.label("Undo");
+			ui.label("Redo");
+			// TODO: Put a line here
+			ui.label("Cut");
+			ui.label("Copy");
+			ui.label("Paste");
+			// TODO: Put a line here
+			ui.label("Find/Replace");
+			ui.label("Select All");
+		});
+	} // create_edit_section
+
+	fn create_run_section(ui : &mut Ui) {
+		ui.button("Run").create_popup(ui, |ui : &mut Ui| {
+			ui.label("Assemble");
+			ui.label("Go");
+			ui.label("Step");
+			ui.label("Backstep");
+			ui.label("Pause");
+			ui.label("Stop");
+			ui.label("Reset");
+			// TODO: Put a line break here
+			ui.label("Clear all breakpoints");
+			ui.label("Toggle all breakpoints");
+		})
+	} // create_run_section
+
+	fn create_settings_section(ui : &mut Ui) {
+		ui.button("Settings").create_popup(ui, |ui : &mut Ui| {
+			let mut boolean : bool = false;
+			
+			ui.checkbox(&mut boolean, "Show Labels Window (symbol table)");
+			ui.checkbox(&mut boolean, "Program arguments provided to wasm program");
+			ui.checkbox(&mut boolean, "Popup dialogue for input syscalls"); // CLARIFY: Does wasm have syscalls?
+			ui.checkbox(&mut boolean, "Addresses displayed in hexadecimal");
+			ui.checkbox(&mut boolean, "Values displayed in hexadecimal");
+			// TODO: Line
+			ui.checkbox(&mut boolean, "Assemble file upon opening");
+			ui.checkbox(&mut boolean, "Assemble all files in directory");
+			ui.checkbox(&mut boolean, "Assembler warnings are considered errors");
+			ui.checkbox(&mut boolean, "Initialize Program Counter to global 'main' if initialized");
+			// Clarify: I don't think any of the last three options make sense here.
+			ui.label("Editor...");
+			ui.label("Highlighting...");
+			ui.label("Exception Handler...");
+			ui.label("Memory Configuration...");
+		})
+	} // create_settings_section
+
 	fn create_tools_section(ui : &mut Ui) {
 		let this_button = ui.button("Tools");
-		let popup_id = ui.next_auto_id();
-		if this_button.contains_pointer() || ui.memory(|mem| mem.is_popup_open(popup_id)) {
-			let anchor = egui::PopupAnchor::ParentRect(this_button.rect);
-			egui::Popup::new(popup_id, ui.ctx().clone(), anchor, this_button.layer_id)
-				.open_memory(egui::SetOpenCommand::Bool(true))
-				.show(|ui : &mut Ui| {
-					ui.button("Item 1");
-					ui.button("Item 2");
-					ui.button("Item 3");
-				}) ;
-		}
-	}
-	
+		this_button.create_popup(ui, |ui : &mut Ui| {
+			ui.label("Nothing here yet!")
+		})
+	} // create_tools_section
+
+	fn create_help_section(ui : &mut Ui) {
+		ui.button("Help").create_popup(ui, |ui : &mut Ui| {
+			ui.label("Help...");
+			ui.label("About...");
+		})
+	} // create_help_section
 	
 } // impl MarsApp
 impl App for MarsApp
